2edf2170c346d2871cd0a0e9fc11613be7b675b4
Evan <evvn@outlook.com>
2020-12-08 21:09:45 +1100

    working Sqlite select app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+import java.sql.*;
+
 public class Main {
 
+    private Connection connect() {
+        String url = "jdbc:sqlite:/Users/evan/uni/bit707_software_engineering/assignment_1/to_do_project/ToDo.db";
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void selectAll() {
+        String sql = "SELECT * FROM ToDo";
+        try (Connection conn = this.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                System.out.println(rs.getInt("taskNumber") + "\t" + rs.getString("taskName"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Main app = new Main();
+        app.selectAll();
     }
 }
